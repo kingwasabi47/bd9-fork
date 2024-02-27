42a674c1b06fa071cb0b247df91f1521a6db6278
kingwasabi47 <guruasab@gmail.com>
2024-02-26 20:03:28 -0800

    created collegeboard.feature and added board.js with xpath

--- page-objects/board.js
+++ page-objects/board.js
@@ -7,12 +7,12 @@ module.exports = {
     url: 'https://prod.idp.collegeboard.org/',
 
     elements: {
-        signInButton: //*[@id="block-cborghomepage"]/div/div[1]/div[3]/div/div[2]/div/div/a,
-        StudentButton: //*[@id="form82"]/div[1]/div[2]/div[2]/div/span/div/label,
-        emailAddress: //*[@id="idp-discovery-username"],
-        RememberEmailAddress: //*[@id="form82"]/div[1]/div[2]/div[2]/div/span/div/label,
-        NextBtn: //*[@id="idp-discovery-submit"],
-        errorMessage: //*[@id="form82"]/div[1]/div[1]/div/div/p,
+        signInButton: '//*[@id="block-cborghomepage"]/div/div[1]/div[3]/div/div[2]/div/div/a',
+        StudentButton: '//*[@id="form82"]/div[1]/div[2]/div[2]/div/span/div/label',
+        emailAddress: '//*[@id="idp-discovery-username"]',
+        RememberEmailAddress: '//*[@id="form82"]/div[1]/div[2]/div[2]/div/span/div/label',
+        NextBtn: '//*[@id="idp-discovery-submit"]',
+        errorMessage: '//*[@id="form82"]/div[1]/div[1]/div/div/p',
         
     },
 
